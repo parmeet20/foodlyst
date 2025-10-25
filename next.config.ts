f63a26c155e84d5e78de587896ef9dcb1a9bbfe9
Parmeet <parmeet202005@gmail.com>
2025-10-25 19:23:32 +0530

    fixed ts errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"], // ✅ add picsum.photos here
   },
